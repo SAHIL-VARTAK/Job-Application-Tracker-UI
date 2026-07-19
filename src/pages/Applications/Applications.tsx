0801bda9e8dd4604d4825617d785fe7de529cb73
SAHIL-VARTAK <svartak2003@gmail.com>
2026-07-19 14:46:05 +0530

    minor addition

--- src/pages/Applications/Applications.tsx
+++ src/pages/Applications/Applications.tsx
@@ -1,13 +1,40 @@
 import { Typography } from "@mui/material";
 
 import AppLayout from "@/components/layout/AppLayout";
+import { useApplications } from "@/hooks/useApplications";
 
 export default function Applications() {
+    const {
+        applications,
+        loading,
+        error,
+    } = useApplications();
+    
+    if (loading) {
+        return ( 
+            <AppLayout>
+                <div>Loading...</div>
+            </AppLayout>
+        );
+    }
+
+    if (error) {
+        return (
+            <AppLayout>
+                <div>{error.detail}</div>
+            </AppLayout>
+        );
+    }
+
     return (
         <AppLayout>
             <Typography variant="h1">
                 Applications
             </Typography>
+            {/*Test code for api integration. Will be removed later*/}
+            <pre>
+                {JSON.stringify(applications, null, 2)}
+            </pre>
         </AppLayout>
     );
 }
